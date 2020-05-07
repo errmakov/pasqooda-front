4909cff6dd3be8354a6895cc7c3ff1d23ff28758
Weblime <weblime@Weblimes-MacBook-Pro.local>
2020-05-07 06:26:27 +0300

    before moving to vuex-persist plugin

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,17 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import("../views/About.vue")
+  },
+  {
+    path: "/testimonials",
+    name: "Testimonials",
+    component: () => import("../views/Testimonials.vue")
+  },
+  {
+    path: "/contacts",
+    name: "Contacts",
+    component: () => import("../views/Contacts.vue")
   }
 ];
 
